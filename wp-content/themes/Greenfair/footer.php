b60a21b643068969bd76b4c5150c056b6846668f
ZeroEllipse <zhvozhvozhvo@gmail.com>
2018-06-19 05:09:36 +0300

    Update footer.php

--- wp-content/themes/Greenfair/footer.php
+++ wp-content/themes/Greenfair/footer.php
@@ -116,6 +116,10 @@
         <script src="<?php echo esc_url (get_template_directory_uri()); ?>/js/bootstrap.min.js"></script>
         <!-- Custom JavaScript-->
         <script src="<?php echo esc_url (get_template_directory_uri()); ?>/js/main.js"></script>
+<?php wp_footer(); ?>
+
     </body>
 
 </html>
+
+
